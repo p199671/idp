3d3b2cb2c501f1f7f0546c6a886c07c9d37a730e
p199671 <paul.wiessner@web.de>
2021-08-26 14:58:10 +0200

    Delete scripts directory

--- scripts/server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash                                                                     
-                                                                                
-echo "Getting $(pos_get_variable hostname) up to date."                         
-apt -y update                                                                   
-apt -y upgrade                                                                  
-                                                                                
-echo "Update & upgrade done." 
