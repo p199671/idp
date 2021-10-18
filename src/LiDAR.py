dc3c6815e42548662e4f45fe81cef2c9feb593cc
p199671 <paul.wiessner@web.de>
2021-10-18 20:21:05 +0200

    Update LiDAR.py

--- src/LiDAR.py
+++ src/LiDAR.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import numpy as np
+from scapy.layers.inet import Ether, IP, UDP
 
 # Flag for Velodyne HDL-32E packets as specified
 # in the manual (https://gpsolution.oss-cn-beijing.aliyuncs.com/manual/LiDAR/MANUAL%2CUSERS%2CHDL32E.pdf)
@@ -42,11 +43,11 @@ def create_velodyne_payload(ranges, intensities, angles, timestamp):
     '''
     This function creates the payload for exactly one packet with HDL-32E Single Return mode data structure
     (as shown in Figure 9-2 of the manual https://gpsolution.oss-cn-beijing.aliyuncs.com/manual/LiDAR/MANUAL%2CUSERS%2CHDL32E.pdf).
-    :param ranges:
-    :param intensities:
-    :param angles:
-    :param timestamp:
-    :return:
+    :param ranges: Range of each measurement in meters where 0 == invalid, (32 x N)
+    :param intensities: Intensity of each measurement where 0 == invalid, (32 x N)
+    :param angles: Angle of each measurement (azimuth) (1 x N)
+    :param timestamp: Real timestamps of each mesaurement (1 x N).
+    :return: Payload for one velodyne hdl-32e packet.
     '''
     payload = b''
 
@@ -81,3 +82,17 @@ def convert_timestamp(timestamp):
     '''
     velodyne_timestamp = timestamp % 3600000000
     return velodyne_timestamp
+
+
+def build_packet(payload, eth_s="60:76:88:20:12:6e", eth_d="ff:ff:ff:ff:ff:ff", ip_s="192.168.1.201", ip_d="255.255.255.255", udp_s=2368, udp_d=2368):
+    '''
+    Builds a packet based on input variables.
+    :param payload: Is either data or position packet payload.
+    :param ip_s: Source IP address
+    :param ip_d: Destination IP address
+    :param udp_s: Source UDP port
+    :param udp_d: Destination UDP port
+    :return: The whole assembled packet.
+    '''
+    packet = Ether(src=eth_s, dst=eth_d)/IP(src=ip_s, dst=ip_d)/UDP(sport=udp_s, dport=udp_d)/payload
+    return packet
